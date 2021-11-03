3af40db439580c665c95955edc3cd1550ab88970
CamilaVitorio <camilasvitorio@hotmail.com>
2021-11-03 11:07:18 -0300

    Conectando ao MongoDB com reposiory e service

---
+++ src/main/java/com/camilavitorio/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.camilavitorio.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.camilavitorio.workshopmongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String>{
+
+}
